a3e10901ba3c94c1b3ac8f256ba2db32dab647a4
madhuri-694 <madhuriperumalla9@gmail.com>
2025-02-28 11:18:02 +0530

    Update College.py

--- College.py
+++ College.py
@@ -59,4 +59,3 @@ if prompt := st.chat_input("Type your question here..."):
                 st.markdown(response.text))
       except Exception as e:
           st.error(f"Sorry, I couldn't generate a response. Error : {e}")
-              st.
